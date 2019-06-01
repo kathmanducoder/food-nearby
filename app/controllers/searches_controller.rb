52e5dcecb0b811e0b31e63bf8b64c81ba1467174
kathmanducoder <kathmanducoder@gmail.com>
2019-06-01 15:23:53 -0400

    Use Faraday to make a query to Foursquare to get list of all the venues

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,6 +3,27 @@ require "pry"
 class SearchesController < ApplicationController
 
   def search
+    begin
+      @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+        req.params['client_id'] = '1CKPP4AE2DRUMBZZUT3JWDRQJXUOLUBM341NRRUY2ONNX2V4'
+        req.params['client_secret'] = 'HJDYSJLLQHLGVDZJFNAM5TUQEYI4VT3BQA0VPECJNO4UIDJA'
+        req.params['v'] = '20160201'
+        req.params['near'] = params[:zipcode]
+        req.params['query'] = 'restaurants'
+      end
+
+      body = JSON.parse(@resp.body)
+      if @resp.success?
+        @venues = body["response"]["venues"]
+      else
+        @error = body["meta"]["errorDetail"]
+      end
+
+    rescue Faraday::ConnectionFailed
+      @error = "There was a timeout. Please try again."
+    end
+
+    render 'index'
   end
 
 end
