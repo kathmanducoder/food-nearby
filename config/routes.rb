658ec548994c8ecc542fcee95ef469f314713b37
kathmanducoder <kathmanducoder@gmail.com>
2019-06-06 16:05:24 -0400

    Add get route for search

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "searches#index"
+  get '/search', to: 'searches#index'
   post '/search', to: 'searches#search'
 end
